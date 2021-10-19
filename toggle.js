2627ff583837f6adcbad03a07ae253c2c6245199
feyisayo1 <45480719+feyisayo1@users.noreply.github.com>
2021-10-19 15:36:48 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -1,22 +1,3 @@
-// const links = document.querySelectorAll(".links");
-// console.log(links);
-
-// const arry = Array.from(links);
-
-
-
-// arry.forEach((links, index) => {
-//     links.addEventListener('click' , () => {
-//         console.log('item clicked ');
-
-//         links.classList.remove('activee');
-//         links.classList.add('activee');
-        
-        
-//     })
-// })
-
-
 const testimonials = document.querySelectorAll('.active__card');
 const dots = document.querySelectorAll('.active__dots');
 
@@ -55,15 +36,3 @@ const counter = setInterval(() => {
 
 
 
-// console.log(main);
-// console.log(testimonials);
-
-
-// give each card the same class called class
-// then in css give in a porperty to make them look blurry 
-// then in js grab the class 
-// then define another class that will caryy opcity class and put it on the first class 
-// then set an interval or set time that maybe after eveyr 1000ms it should change and set a counter 
-// then counter 0 and increase the couinter by one in the loop 
-// then set the active card with the counter 
-// set the active 
