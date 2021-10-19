bc07ba44abfa506e1f61937bfd1e3b845f0b4555
feyisayo1 <45480719+feyisayo1@users.noreply.github.com>
2021-10-19 22:47:15 +0100

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -32,16 +32,14 @@ const counter = setInterval(() => {
 
 
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function (){
-    if (prevScrollpos > currentScrollPos){
-        document.getElementById("navbar).style.top = "0";
-
-    }else{
-     document.getElementById("navbar').style.top = "-75px";
-        }
-    prevScrollpos = currentScrollPos;
- }
-
-
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-75px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
